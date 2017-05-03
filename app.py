c9e97b16719809138d6b7f0448d65f3b8d533776
4sakeninfinity <forsaken.infinity@gmail.com>
2017-05-03 16:13:06 -0500

    Initialize a basic flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
